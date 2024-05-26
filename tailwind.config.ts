007ac99d0581ffcd4865267d227aa32b71439367
Raumil D <rdhandhu@asu.edu>
2024-05-26 15:20:55 -0700

    Added Macbook Scroll Animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const svgToDataUri = require("mini-svg-data-uri");
-
+const defaultTheme = require("tailwindcss/defaultTheme");
 const colors = require("tailwindcss/colors");
 const {
   default: flattenColorPalette,
