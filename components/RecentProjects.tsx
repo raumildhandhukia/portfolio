f84b6986a08456209ccdb0fea36de55e934af47f
Raumil D <rdhandhu@asu.edu>
2024-05-26 22:59:10 -0700

    GIF for projects

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -24,7 +24,7 @@ const RecentProjects = () => {
             key={item.id}
           >
             <PinContainer title={item.title} href={item.link}>
-              <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
+              <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] mb-10">
                 <div
                   className="relative w-full h-full overflow-hidden lg:rounded-3xl"
                   style={{ backgroundColor: "#13162D" }}
